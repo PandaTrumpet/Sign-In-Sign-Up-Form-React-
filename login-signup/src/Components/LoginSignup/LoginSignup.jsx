602259fa7c3d84e4a3a5038b457a73853b2d69e5
PandaTrumpet <dsavontrumpet@gmail.com>
2024-11-09 21:55:23 +0100

    finish project

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,37 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./LoginSignup.module.css";
+import clsx from "clsx";
 import userIcon from "../Assets/person.png";
 import emailIcon from "../Assets/email.png";
 import passwordIcon from "../Assets/password.png";
 const LoginSignup = () => {
+  const [action, setAction] = useState("Login");
+
   return (
     <div>
       <div className={css.container}>
         <div className={css.header}>
-          <div className={css.text}>Sign Up</div>
+          <div className={css.text}>{action}</div>
           <div className={css.underline}></div>
         </div>
 
         <div className={css.inputs}>
+          {action === "Login" ? (
+            <div></div>
+          ) : (
+            <div className={css.input}>
+              <img src={userIcon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+          )}
+
           <div className={css.input}>
-            <img src={userIcon} alt="" />
-            <input type="text" />
-          </div>
-          <div className={css.inputs}>
             <img src={emailIcon} alt="" />
-            <input type="email" />
+            <input type="email" placeholder="Email Id" />
           </div>
-          <div className={css.inputs}>
+          <div className={css.input}>
             <img src={passwordIcon} alt="" />
-            <input type="password" />
+            <input type="password" placeholder="Password" />
           </div>
         </div>
-        <div className={css.forgotPassword}>
-          Lost Password? <span>Click Here!</span>
-        </div>
+        {action === "Sign Up" ? (
+          <div></div>
+        ) : (
+          <div className={css.forgotPassword}>
+            Lost Password? <span>Click Here!</span>
+          </div>
+        )}
+
         <div className={css.submitContainer}>
-          <div className="submit">Sign Up</div>
-          <div className="submit">Login</div>
+          <div
+            className={clsx(action === "Login" ? css.gray : "", css.submit)}
+            onClick={() => {
+              setAction("Sign Up");
+            }}
+          >
+            Sign Up
+          </div>
+          <div
+            className={clsx(action === "Sign Up" ? css.gray : "", css.submit)}
+            onClick={() => {
+              setAction("Login");
+            }}
+          >
+            Login
+          </div>
         </div>
       </div>
     </div>
